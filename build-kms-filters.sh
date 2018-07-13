05d6a88ec41745fa58646b53ee6456c6a37b5af1
Mario Jr <mariogasparoni@gmail.com>
2018-07-12 23:17:35 -0300

    Do not install kmsjsoncpp and openwebrtc-gst-plugins
    The same way we do with other deps, we build and load it locally

--- build-kms-filters.sh
+++ build-kms-filters.sh
@@ -9,6 +9,8 @@ KMS_CMAKE_UTILS_DIR=$ROOT/kms-cmake-utils
 KURENTO_MODULE_CREATOR_DIR=$ROOT/kurento-module-creator
 KMS_JSONRPC_DIR=$ROOT/kms-jsonrpc
 KMS_JSONCPP_DIR=$ROOT/jsoncpp
+OPENWEBRTC_GST_PLUGINS_DIR=$ROOT/openwebrtc-gst-plugins
+
 #Run build-kms-core.sh if the above modules aren't built
 
 LIBRARY_PATH="$KMS_JSONCPP_DIR/src/lib_json";
@@ -23,6 +25,10 @@ sudo apt-get install --no-install-recommends -y libopencv-dev
 #Replace GST 1.5 version to 1.0 (xenial's default)
 find $KMS_FILTERS_DIR -name CMakeLists.txt -print0 | xargs -0 sed -i -e "s/gstreamer\([a-zA-Z0-9-]*\)1.5/gstreamer\11.0/g"
 
+PKG_CONFIG_PATH="$OPENWEBRTC_GST_PLUGINS_DIR:$KMS_JSONCPP_DIR/pkg-config/";
+CMAKE_CXX_FLAGS=" -I$KMS_JSONCPP_DIR/include/"
+CMAKE_C_FLAGS="$CMAKE_CXX_FLAGS"
+
 sed -i -e "s/gst\([a-zA-Z0-9-]*\)1.5/gst\11.0/g" $KMS_FILTERS_DIR/debian/control
 sed -i -e "s/gst\([a-zA-Z0-9-]*\)1.5/gst\11.0/g" $KMS_FILTERS_DIR/debian/kms-filters.install
 
@@ -32,6 +38,6 @@ KURENTO_MODULES_DIR="$KMS_CORE_DIR/src/server/kmd;$KMS_ELEMENTS_DIR/src/server/k
 CMAKE_MODULE_PATH="$KMS_CMAKE_UTILS_DIR;$KMS_CMAKE_UTILS_DIR/CMake;$KURENTO_MODULE_CREATOR_DIR/classes;$KMS_CORE_DIR;$KMS_CORE_DIR/CMake;$KMS_CORE_DIR/src/server;$KMS_CORE_DIR/src/gst-plugins;$KMS_CORE_DIR/src/gst-plugins/commons;$KMS_JSONRPC_DIR;$KMS_JSONRPC_DIR/src;$KMS_ELEMENTS_DIR/src/server"
 CMAKE_PREFIX_PATH="$KURENTO_MODULE_CREATOR_DIR;$KMS_CORE_DIR;$KMS_CORE_DIR/src/server;$KMS_CORE_DIR/src/gst-plugins;$KMS_CORE_DIR/src/gst-plugins/commons;$KMS_JSONRPC_DIR;$KMS_JSONRPC_DIR/src;$KMS_JSONRPC_DIR/src/jsoncpp;$KMS_ELEMENTS_DIR";
 
-cmake -DKURENTO_MODULES_DIR=$KURENTO_MODULES_DIR -DCMAKE_MODULE_PATH=$CMAKE_MODULE_PATH -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH .
+env PKG_CONFIG_PATH=$PKG_CONFIG_PATH cmake -DKURENTO_MODULES_DIR=$KURENTO_MODULES_DIR -DCMAKE_MODULE_PATH=$CMAKE_MODULE_PATH -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" .
 env LIBRARY_PATH=$LIBRARY_PATH make
 cd $ROOT
