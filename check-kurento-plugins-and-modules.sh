0c2fe93773d8923f91210e7623d3a3e2cd39ba3b
Mario Jr <mariogasparoni@gmail.com>
2020-03-02 12:22:47 -0300

    Fix gstreamer plugin checker LD_LIBRARY_PATH

--- check-kurento-plugins-and-modules.sh
+++ check-kurento-plugins-and-modules.sh
@@ -4,6 +4,8 @@ ROOT=`pwd`
 KMS_CORE_DIR=$ROOT/kms-core
 KMS_FILTERS_DIR=$ROOT/kms-filters
 KMS_ELEMENTS_DIR=$ROOT/kms-elements
+KMS_JSONRPC_DIR=$ROOT/kms-jsonrpc
+KMS_JSONCPP_DIR=$ROOT/jsoncpp
 OPENWEBRTC_GST_PLUGINS_DIR=$ROOT/openwebrtc-gst-plugins
 
 GST_PLUGIN_PATH="$KMS_CORE_DIR/server:$KMS_CORE_DIR/src/gst-plugins:$KMS_CORE_DIR/src/gst-plugins/commons:$KMS_ELEMENTS_DIR/src/server:$KMS_ELEMENTS_DIR/src/gst-plugins:$KMS_FILTERS_DIR/src/server:$KMS_FILTERS_DIR/src/gst-plugins";
@@ -14,4 +16,4 @@ LIBRARY_PATH="/usr/local/lib:$KMS_JSONCPP_DIR/src/lib_json:$KMS_JSONRPC_DIR/src:
 #Requires gstreamer1.0-tools package
 #sudo apt-get install gstreamer1.0-tools
 echo "GStreamer Plugins"
-env LD_LIBRARY_PATH=$LD_LIBRARY_PATH GST_PLUGIN_PATH=$GST_PLUGIN_PATH gst-inspect-1.0
+env LD_LIBRARY_PATH=$LIBRARY_PATH GST_PLUGIN_PATH=$GST_PLUGIN_PATH gst-inspect-1.0
